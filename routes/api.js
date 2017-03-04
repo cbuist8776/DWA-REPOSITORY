8fbc8c9562c71dc82ef93cc762553d1cb60bb4f4
cbuist8776 <chadscontacts2014@gmail.com>
2017-03-03 19:02:06 -0500

    Code Commenting Edited

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,37 @@
+//router and path variables set to work with express and the path being required
+
 module.exports = function (express) {
   var router = express.Router();
   var path = require("path");
 
+
+// Endpoint of api/v1/urls set to post with the requirement and resportions set in the function. Console.log to check
+
   router.post('/api/v1/urls', function(req, res){
     console.log('test');
-    
+
+//Variable string set to strSource to make the shortened link from.
+
  var strSource = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789'
+
+//For Loop set to have i = 0 and to increment with Math.round and Math.random functions to help shorten the url.
 
 var strReturn = '';
 for (var i=0 ; i<strLength; i++){
 	var rand = Math.round(Math.random() * (strSource.length - 1))
 	strReturn += strSource.charAt(rand);
-	
-	
+
+//json information set to accept the url that is entered and return the variable strReturn for shortned url.
+
+
 	res.json({URLLink:'http://' + strReturn});
-	
+
 	};
-	
+
+//router set to get the file path api/v1/urls to show the shortned url.
+
 
 	router.get('/api/v1/urls', function (req, res) {
 		res.sendFile(path.join(__dirname+'/views/index.html'));
-		
+
 	});
-		
-		
-		
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+// Express, body_parser, and app variables set.
+
 var express = require('express');
 var body_parser = require('body-parser');
 var app = express();
@@ -10,7 +12,7 @@ app.use('/api', require('../routes/api.js')(express));
 
 var util = require('./util.js');
 
-// Call random generator - return string with 6 characters. 
+// Call random generator - return string with 6 characters.
 
 var output = util.stringGen(6);
 
